a03f6a0b4847841a957db27cb7418f2ff339d321
davidwasamrf <david.wasa@marfeel.com>
2021-06-17 12:06:52 +0200

    I think I nailed it

--- src/main/java/org/wasa/scoreranking/Scores.java
+++ src/main/java/org/wasa/scoreranking/Scores.java
@@ -1,6 +1,42 @@
 package org.wasa.scoreranking;
 
-public class Scores {
+import lombok.Value;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import static java.util.Collections.binarySearch;
+
+class Scores {
+
+    private final Map<String, Double> scoreByName = new HashMap<>();
+    private final List<Member> sortedMembers = new ArrayList<>();
+
+    void addMember(String name, Double score) {
+        scoreByName.put(name, score);
+
+        sortedMembers.add(Member.of(name, score));
+        Collections.sort(sortedMembers);
+    }
+
+    int getRank(String name) {
+        Double score = scoreByName.get(name);
+
+        return binarySearch(sortedMembers, new Member(name, score));
+    }
+
+    @Value(staticConstructor = "of")
+    private static class Member implements Comparable<Member> {
+        String name;
+        Double score;
+
+        @Override
+        public int compareTo(Member other) {
+            int scoreComparison = - getScore().compareTo(other.getScore());
+            return scoreComparison != 0 ? scoreComparison : getName().compareTo(other.getName());
+        }
+    }
 }
--- src/test/java/org/wasa/scoreranking/ScoresTest.java
+++ src/test/java/org/wasa/scoreranking/ScoresTest.java
@@ -1,8 +1,26 @@
 package org.wasa.scoreranking;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ScoresTest {
 
+    @Test
+    public void rocksRankingAtSomeGameOnTheInternets() {
+        Scores scores = new Scores();
+
+        scores.addMember("Ale", 0D);
+        scores.addMember("Roger", 10D);
+        scores.addMember("Jordi", 20D);
+        scores.addMember("David", 30D);
+        scores.addMember("David", 50D);
+        scores.addMember("Ale", 60D);
+        scores.addMember("Oscar", 70D);
+
+        assertEquals(1, scores.getRank("Ale"),
+                "Ale is the second best, so rank is 1 since it starts at 0. "
+                        + "Also, Ale also has a pretty terrible score, but best score is taken into account");
+    }
 
 }
